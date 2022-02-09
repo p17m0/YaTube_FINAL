212485a3234dc5a1c3af1867a3a64bcdf76c7314
Matvey Dorofeev <matiaso@bk.ru>
2022-02-09 15:59:34 +0300

    @

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 
 from django.urls import include, path
 
-handler404 = 'core.views.page_not_found'
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
